264769d1ba1888e458adc50c38f071291c52f14a
FloorboardMan <32313229+FloorboardMan@users.noreply.github.com>
2022-08-25 21:41:34 +0700

    Changes to bypass cors

--- src/main/java/main/lookup/controller/LookupController.java
+++ src/main/java/main/lookup/controller/LookupController.java
@@ -2,10 +2,7 @@ package main.lookup.controller;
 
 import main.lookup.Lookup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/lookup")
@@ -15,11 +12,13 @@ public class LookupController {
    Lookup lookup;
 
     @GetMapping("/english/{englishWord}")
+    @ResponseBody
     public String lookupEnglish(@PathVariable String englishWord) {
         return lookup.lookupIndonesianWord(englishWord);
     }
 
     @GetMapping("/indonesian/{indonesianWord}")
+    @ResponseBody
     public String lookupIndonesian(@PathVariable String indonesianWord) {
         return lookup.lookupEnglishWord(indonesianWord);
     }
