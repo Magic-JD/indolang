a9392aed785d27f3fd3ef173beeec8c25c10aa79
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-01 16:35:39 +0700

    Check that the name doesn't already exist before adding a new user

--- src/main/java/main/registration/RegistrationResource.java
+++ src/main/java/main/registration/RegistrationResource.java
@@ -5,10 +5,10 @@ import main.database.repository.UserRepository;
 import main.registration.data.UserCredentialsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
@@ -23,9 +23,14 @@ public class RegistrationResource {
 
 
     @PostMapping("/registration")
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public void register(@RequestBody UserCredentialsDto userCredentialsDto) {
-        DbUserDetails user = new DbUserDetails(userCredentialsDto.getUsername(), passwordEncoder.encode(userCredentialsDto.getPassword()), true, Set.of("USER"));
-        userRepository.save(user);
+    public ResponseEntity register(@RequestBody UserCredentialsDto userCredentialsDto) {
+        if (userRepository.findItemByName(userCredentialsDto.getUsername()) == null) {
+            DbUserDetails user = new DbUserDetails(userCredentialsDto.getUsername(), passwordEncoder.encode(userCredentialsDto.getPassword()), true, Set.of("USER"));
+            userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
     }
 }
