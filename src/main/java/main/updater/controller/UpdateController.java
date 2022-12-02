397de05b572a27b6e9117199159fa54929530d94
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-02 18:06:59 +0700

    Updated the UpdateController

--- src/main/java/main/updater/controller/UpdateController.java
+++ src/main/java/main/updater/controller/UpdateController.java
@@ -4,7 +4,7 @@ import main.updater.DatabaseUpdater;
 import main.updater.data.Definition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +15,11 @@ public class UpdateController {
     DatabaseUpdater databaseUpdater;
 
     @PostMapping("/dictionary")
-    public ResponseEntity<Boolean> addWordToDictionary(@RequestBody Definition definition,
-                                                       @RequestHeader(HttpHeaders.ACCEPT_LANGUAGE) String language) {
-        return ResponseEntity.ok(databaseUpdater.updateDatabase(definition, language));
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addWordToDictionary(@RequestBody Definition definition,
+                                    @RequestHeader(HttpHeaders.ACCEPT_LANGUAGE) String language) {
+        databaseUpdater.updateDatabase(definition, language);
+
     }
 
 }
