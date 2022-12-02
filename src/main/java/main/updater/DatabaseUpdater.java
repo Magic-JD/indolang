6c91a2aab98a092b6f91a887e93a6b6b137579ab
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-02 18:04:32 +0700

    Fixed a bug in DatabaseUpdater.java

--- src/main/java/main/updater/DatabaseUpdater.java
+++ src/main/java/main/updater/DatabaseUpdater.java
@@ -6,25 +6,16 @@ import main.updater.data.Definition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
-
 @Component
 public class DatabaseUpdater {
 
     @Autowired
     WordTranslationsRepository repository;
 
-    public boolean updateDatabase(Definition definition, String language) {
-        return repository.findByKeyword(definition.getWord(), language)
-                .map(item -> updateTranslations(item, definition.getTranslation()))
-                .map(repository::save)
-                .isPresent();
-    }
-
-    private DbWordTranslationsItem updateTranslations(DbWordTranslationsItem item, String newWord) {
-        Set<String> translations = item.getTranslations();
-        translations.add(newWord);
-        item.setTranslations(translations);
-        return item;
+    public void updateDatabase(Definition definition, String language) {
+        DbWordTranslationsItem item = repository.findByKeyword(definition.getWord(), language)
+                .orElse(new DbWordTranslationsItem(null, language, definition.getWord()));
+        item.addToTranslations(definition.getTranslation());
+        repository.save(item);
     }
 }
