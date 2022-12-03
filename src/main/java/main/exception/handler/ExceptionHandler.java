5d4d431228da17a7964a9c9e951b77cdf7200d95
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-03 18:17:38 +0700

    Renaming ExceptionHandler.java and associated package

--- src/main/java/main/exception/handler/ExceptionHandler.java
+++ src/main/java/main/exception/handler/ExceptionHandler.java
@@ -1,39 +1,38 @@
-package main.exception.controller;
+package main.exception.handler;
 
 import main.exception.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController {
-    @ExceptionHandler(Exceptions.LanguageNotSupportedException.class)
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.LanguageNotSupportedException.class)
     public ResponseEntity<Object> exception(Exceptions.LanguageNotSupportedException exception) {
         return new ResponseEntity<>("This language is currently not supported", HttpStatus.NOT_ACCEPTABLE);
     }
 
-    @ExceptionHandler(Exceptions.LanguageNotProvidedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.LanguageNotProvidedException.class)
     public ResponseEntity<Object> exception(Exceptions.LanguageNotProvidedException exception) {
         return new ResponseEntity<>("The language is not provided", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exceptions.WordNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.WordNotFoundException.class)
     public ResponseEntity<Object> exception(Exceptions.WordNotFoundException exception) {
         return new ResponseEntity<>("The provided word could not be found in the dictionary.", HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
-    @ExceptionHandler(Exceptions.AllWordsLearnedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.AllWordsLearnedException.class)
     public ResponseEntity<Object> exception(Exceptions.AllWordsLearnedException exception) {
         return new ResponseEntity<>("User has learned all the possible words.", HttpStatus.PARTIAL_CONTENT);
     }
 
-    @ExceptionHandler(Exceptions.TranslationsNotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.TranslationsNotFoundException.class)
     public ResponseEntity<Object> exception(Exceptions.TranslationsNotFoundException exception) {
         return new ResponseEntity<>("Translations for the given question word could not be found.", HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
-    @ExceptionHandler(Exceptions.UserHasNotLearnedWordException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exceptions.UserHasNotLearnedWordException.class)
     public ResponseEntity<Object> exception(Exceptions.UserHasNotLearnedWordException exception) {
         return new ResponseEntity<>("The user has not yet learned this word, or it has been removed from the database.", HttpStatus.UNPROCESSABLE_ENTITY);
     }
