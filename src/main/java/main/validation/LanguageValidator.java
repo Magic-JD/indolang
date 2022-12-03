26c8ddf32b98a7d692efdd841bbb8416488450a0
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-03 18:14:42 +0700

    Using spring utils

--- src/main/java/main/validation/LanguageValidator.java
+++ src/main/java/main/validation/LanguageValidator.java
@@ -3,6 +3,7 @@ package main.validation;
 import main.exception.Exceptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Set;
 
@@ -13,7 +14,7 @@ public class LanguageValidator {
     private Set<String> supportedLanguages;
 
     public void validateLanguage(String language) {
-        if ("".equals(language)) {
+        if (StringUtils.hasText(language)) {
             throw new Exceptions.LanguageNotProvidedException();
         }
         if (!supportedLanguages.contains(language)) {
