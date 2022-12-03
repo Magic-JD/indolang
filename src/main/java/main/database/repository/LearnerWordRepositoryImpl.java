62ef55fe0a6ff49ac11e0276f45f82f15a7f24f0
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-03 17:13:07 +0700

    Use var

--- src/main/java/main/database/repository/LearnerWordRepositoryImpl.java
+++ src/main/java/main/database/repository/LearnerWordRepositoryImpl.java
@@ -1,7 +1,6 @@
 package main.database.repository;
 
 import main.database.model.DbWordTranslationsItem;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Component
 public class LearnerWordRepositoryImpl implements LearnerWordRepository {
@@ -19,7 +17,7 @@ public class LearnerWordRepositoryImpl implements LearnerWordRepository {
 
     @Override
     public Optional<DbWordTranslationsItem> findNewQuestion(String username, String language) {
-        Stream<ObjectId> objectIdStream = learnerRepository.findAllForLearner(username).stream().map(dbl -> dbl.getWordTranslation().get_id());
+        var objectIdStream = learnerRepository.findAllForLearner(username).stream().map(dbl -> dbl.getWordTranslation().get_id());
         var query = new Query(Criteria.where("locale").is(language).and("_id").nin(objectIdStream));
         return Optional.ofNullable(mongoTemplate.findOne(query, DbWordTranslationsItem.class));
     }
