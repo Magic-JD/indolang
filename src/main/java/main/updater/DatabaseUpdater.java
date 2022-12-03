39d31e71c339fb45bb23b07e2456c6533872842d
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-03 17:35:42 +0700

    Extracting interfaces

--- src/main/java/main/updater/DatabaseUpdater.java
+++ src/main/java/main/updater/DatabaseUpdater.java
@@ -1,20 +1,7 @@
 package main.updater;
 
-import main.database.model.DbWordTranslationsItem;
-import main.database.repository.WordTranslationsRepository;
 import main.updater.data.Definition;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
-public class DatabaseUpdater {
-
-    @Autowired WordTranslationsRepository repository;
-
-    public void updateDatabase(Definition definition, String language) {
-        DbWordTranslationsItem item = repository.findByKeyword(definition.getWord(), language)
-                .orElse(new DbWordTranslationsItem(language, definition.getWord()));
-        item.addToTranslations(definition.getTranslation());
-        repository.save(item);
-    }
+public interface DatabaseUpdater {
+    void updateDatabase(Definition definition, String language);
 }
