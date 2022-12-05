5304301e16ddcf9185319dfcdf4866d1d19f9472
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-05 14:30:44 +0700

    Removing the use of magic strings in code.

---
+++ src/main/java/main/exception/constants/ExceptionConstants.java
@@ -0,0 +1,11 @@
+package main.exception.constants;
+
+public class ExceptionConstants {
+    public static final String LANGUAGE_NOT_SUPPORTED = "This language is currently not supported";
+    public static final String LANGUAGE_NOT_PROVIDED = "The language is not provided";
+    public static final String WORD_COULD_NOT_BE_FOUND = "The provided word could not be found in the dictionary.";
+    public static final String USER_LEARNED_ALL_WORDS = "User has learned all the possible words.";
+    public static final String NO_TRANSLATIONS_FOUND = "Translations for the given question word could not be found.";
+    public static final String USER_HAS_NOT_LEARNED_WORD = "The user has not yet learned this word, or it has been removed from the database.";
+    public static final String WORD_CANNOT_BE_REMOVED = "The word you want to remove does not exist";
+}
--- src/main/java/main/exception/handler/ExceptionHandlers.java
+++ src/main/java/main/exception/handler/ExceptionHandlers.java
@@ -6,40 +6,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static main.exception.constants.ExceptionConstants.*;
+
 @ControllerAdvice
 public class ExceptionHandlers {
+
     @ExceptionHandler(Exceptions.LanguageNotSupportedException.class)
     public ResponseEntity<Object> exception(Exceptions.LanguageNotSupportedException exception) {
-        return new ResponseEntity<>("This language is currently not supported", HttpStatus.NOT_ACCEPTABLE);
+        return new ResponseEntity<>(LANGUAGE_NOT_SUPPORTED, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(Exceptions.LanguageNotProvidedException.class)
     public ResponseEntity<Object> exception(Exceptions.LanguageNotProvidedException exception) {
-        return new ResponseEntity<>("The language is not provided", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(LANGUAGE_NOT_PROVIDED, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exceptions.WordNotFoundException.class)
     public ResponseEntity<Object> exception(Exceptions.WordNotFoundException exception) {
-        return new ResponseEntity<>("The provided word could not be found in the dictionary.", HttpStatus.UNPROCESSABLE_ENTITY);
+        return new ResponseEntity<>(WORD_COULD_NOT_BE_FOUND, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(Exceptions.AllWordsLearnedException.class)
     public ResponseEntity<Object> exception(Exceptions.AllWordsLearnedException exception) {
-        return new ResponseEntity<>("User has learned all the possible words.", HttpStatus.PARTIAL_CONTENT);
+        return new ResponseEntity<>(USER_LEARNED_ALL_WORDS, HttpStatus.PARTIAL_CONTENT);
     }
 
     @ExceptionHandler(Exceptions.TranslationsNotFoundException.class)
     public ResponseEntity<Object> exception(Exceptions.TranslationsNotFoundException exception) {
-        return new ResponseEntity<>("Translations for the given question word could not be found.", HttpStatus.UNPROCESSABLE_ENTITY);
+        return new ResponseEntity<>(NO_TRANSLATIONS_FOUND, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(Exceptions.WordNotLearnedException.class)
     public ResponseEntity<Object> exception(Exceptions.WordNotLearnedException exception) {
-        return new ResponseEntity<>("The user has not yet learned this word, or it has been removed from the database.", HttpStatus.UNPROCESSABLE_ENTITY);
+        return new ResponseEntity<>(USER_HAS_NOT_LEARNED_WORD, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(Exceptions.WordDoesNotExistToBeRemoved.class)
     public ResponseEntity<Object> exception(Exceptions.WordDoesNotExistToBeRemoved exception) {
-        return new ResponseEntity<>("The word you want to remove does not exist", HttpStatus.UNPROCESSABLE_ENTITY);
+        return new ResponseEntity<>(WORD_CANNOT_BE_REMOVED, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
