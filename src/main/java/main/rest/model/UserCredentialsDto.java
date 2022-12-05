28f43d1316776ec28caa9e7df66b28847fcfc588
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2022-12-05 17:36:49 +0700

    Removing unnecessary information

--- src/main/java/main/rest/model/UserCredentialsDto.java
+++ src/main/java/main/rest/model/UserCredentialsDto.java
@@ -2,11 +2,8 @@ package main.rest.model;
 
 import lombok.Data;
 
-import java.util.Set;
-
 @Data
 public class UserCredentialsDto {
     private final String username;
     private final String password;
-    private final Set<String> roles;
 }
